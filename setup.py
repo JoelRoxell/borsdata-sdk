60923463036b4542d1de7515b41fbc56e3d7c9b8
Joel Roxell <joel.roxell@annevo.se>
2019-04-22 14:31:49 +0200

    requirements: added file.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="borsdata-sdk",
-    version="0.0.5",
+    version="0.0.1",
     author="Joel S. Roxell",
     author_email="joel.roxell@annevo.se",
     description="Borsdata api SDK",
