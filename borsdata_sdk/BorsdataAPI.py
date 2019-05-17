046022596487e73a7f3c02badb6f2d5521d690ce
Joel Roxell <joel.roxell@annevo.se>
2019-05-17 12:21:37 +0200

    BorsdataAPI: added docs.

--- borsdata_sdk/BorsdataAPI.py
+++ borsdata_sdk/BorsdataAPI.py
@@ -13,13 +13,11 @@ from .models.StockPrice import StockPrice
 from .models.Country import Country
 
 RATE_LIMIT = 429
+RATE_WAIT = .3
 UPDATED = True
 
 
 class BorsdataAPI:
-    _markets: List[Market]
-    _instruments: List[Instrument]
-
     def __init__(self, api_key):
         self.api_key = api_key
         self._params = {'authKey': self.api_key}
@@ -27,17 +25,14 @@ class BorsdataAPI:
         self._version = 'v1'
         self._root = '{host}/{version}/'.format(
             host=self._uri, version=self._version)
-        self._markets = None
-        self._instruments = None
-
-    @property
-    def markets(self) -> List[Market]:
-        if self._markets is None:
-            self._markets = self.get_markets()
-
-        return self._markets
 
     def get_markets(self) -> List[Market]:
+        """Returns all markets.
+
+        Returns:
+            List[Market] -- List of markets.
+        """
+
         self._markets = [Market(**market)
                          for market in self._get_data_object('markets')]
 
@@ -46,13 +41,28 @@ class BorsdataAPI:
     def get_branches(self):
         return [Branch(**branch) for branch in self._get_data_object('branches')]
 
-    def get_sectors(self):
+    def get_sectors(self) -> List[Sector]:
+        """Returns all sectors.
+
+        Returns:
+            List[Sector] -- List of sectors.
+        """
+
         return [Sector(**sector) for sector in self._get_data_object('sectors')]
 
     def get_countries(self):
         return [Country(**country) for country in self._get_data_object('countries')]
 
     def get_instruments(self, markets=None) -> List[Instrument]:
+        """Returns all instruments.
+
+        Keyword Arguments:
+            markets {int[]} -- Id list of markets used to filter the resulting list (default: {None}).
+
+        Returns:
+            List[Instrument] -- List of instruments.
+        """
+
         instruments = [Instrument(**instrument)
                        for instrument in self._get_data_object('instruments')]
 
@@ -68,11 +78,33 @@ class BorsdataAPI:
         return filtered_instruments
 
     def get_instruments_updated(self) -> List[InstrumentUpdate]:
+        """Returns all updated instruments.
+
+        Returns:
+            List[InstrumentUpdate] -- List of all updated instruments.
+        """
+
         _, data = self._get('instruments/updated')
 
         return [InstrumentUpdate(**instrument) for instrument in data.get('instruments')]
 
-    def get_instrument_stock_price(self, ins_id, start=None, end=None):
+    def get_instrument_stock_price(self, ins_id, start=None, end=None) -> List[StockPrice]:
+        """Returns stockprices for an instrument (ins_id), it is possible to determine the timespan using start and end. Max 10 years, if no time filters provided.
+
+        Arguments:
+            ins_id {int} -- Instrument id.
+
+        Keyword Arguments:
+            start {str} --  Determines from which day to start the collection, ex: '2009-04-22' (default: {None})
+            end {str} -- Determines the collection limit, ex:'2009-04-25' (default: {None})
+
+        Raises:
+            IOError: Is thrown if the API responds with anything other than 200.
+
+        Returns:
+            List[StockPrice] -- List of the collected stockprices.
+        """
+
         params = self._params.copy()
 
         while True:
@@ -97,13 +129,29 @@ class BorsdataAPI:
         return entries
 
     def get_instrument_stock_price_last(self) -> List[StockPrice]:
-        """ Returns Last StockPrices for all instruments.
+        """Returns Last StockPrices for all instruments.
+
+        Returns:
+            List[StockPrice] -- List of all last updated price entries for each instrument.
         """
+
         status, data = self._get('instruments/stockprices/last')
 
         return [StockPrice(**entry) for entry in data.get('stockPricesList', [])]
 
     def _get_data_object(self, data_type):
+        """Gets the specified datatype from the remote API.
+
+        Arguments:
+            data_type {str} -- Datatype.
+
+        Raises:
+            IOError: Is thrown if the API responds with anything other than 200.
+
+        Returns:
+            List[T] - List of the passed type.
+        """
+
         status, data = self._get(data_type)
 
         if status != 200:
@@ -112,13 +160,22 @@ class BorsdataAPI:
 
         return data.get(data_type)
 
-    def _get(self, endpoint):
+    def _get(self, endpoint) -> (int, dict):
+        """Perform a remote request to the API and retries on rate limit responses, returns json payload on success.
+
+        Arguments:
+            endpoint {str} -- endpoint to access, ex: `instruments/StockSplits`.
+
+        Returns:
+            (int, dict) -- The first entry, holds the status code of the response; the second, holds the response content parsed from json.
+        """
+
         while True:
             res = get(self._root + endpoint, self._params, verify=False)
 
             if res.status_code != RATE_LIMIT:
                 break
 
-            sleep(0.3)
+            sleep(RATE_WAIT)
 
         return res.status_code, res.json()
